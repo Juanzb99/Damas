6dd3b47b4b1004518f483cfb5ba78ec390fc6c0c
Juan <62060414+Juanzb99@users.noreply.github.com>
2020-05-22 13:00:42 +0200

    Add files via upload

---
+++ src/Damas.cpp
@@ -0,0 +1,67 @@
+#include "glut.h"
+
+
+void OnDraw(void); //esta funcion sera llamada para dibujar
+void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
+void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla
+
+int main(int argc, char* argv[])
+{
+	//Inicializar el gestor de ventanas GLUT
+	glutInit(&argc, argv);
+	glutInitWindowSize(800, 600);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("DAMAS");
+
+	//habilitar luces y definir perspectiva
+	glEnable(GL_LIGHT0);
+	glEnable(GL_LIGHTING);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_COLOR_MATERIAL);
+	glMatrixMode(GL_PROJECTION);
+	gluPerspective(40.0, 800 / 600.0f, 0.1, 150);
+
+	//Registrar los callbacks
+	glutDisplayFunc(OnDraw);
+	glutTimerFunc(25, OnTimer, 0);
+	glutKeyboardFunc(OnKeyboardDown);
+
+	//pasarle el control a GLUT,que llamara a los callbacks
+	glutMainLoop();
+
+	return 0;
+}
+
+void OnDraw(void)
+{
+	//Borrado de la pantalla	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Para definir el punto de vista
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(0.0, 10, 20,  // posicion del ojo
+		0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
+		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
+
+	//aqui es donde hay que poner el código de dibujo
+	glutSolidCube(3);
+
+	//no borrar esta linea ni poner nada despues
+	glutSwapBuffers();
+}
+
+void OnKeyboardDown(unsigned char key, int x_t, int y_t)
+{
+
+	glutPostRedisplay();
+}
+
+void OnTimer(int value)
+{
+
+	glutPostRedisplay(); //necesario poner esto (para que se vuelva a dibujar o algo asi)
+	glutTimerFunc(25, OnTimer, 0);
+}
+
