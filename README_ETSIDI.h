5d59f38437e15511bb2dc5eb9344bad5b3822e98
Juan <62060414+Juanzb99@users.noreply.github.com>
2020-06-05 00:12:41 +0200

    Create README_ETSIDI.h

---
+++ README_ETSIDI.h
@@ -0,0 +1,2 @@
+La libreria ETSIDI.h utilizada en este proyecto da problemas a la hora del clonado del repositorio. Como solución alternativa se deberá
+configurar el proyecto creado añadiendo la libreria manualmente.
